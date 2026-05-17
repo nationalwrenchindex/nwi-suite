f96e16a2c21f0300b7a56a5840b28d844edb37ad
Edward Fleeman <edwardfleeman57@gmail.com>
2026-05-17 01:37:59 -0400

    feat(monitoring): install Sentry SDK with error tracking and logging

---
+++ sentry.server.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a2ebd9a68dafc866a146734cf82e95dd@o4511403413340160.ingest.us.sentry.io/4511403500503040",
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
